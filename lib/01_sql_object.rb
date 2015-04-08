e104327490a4b3020f843f1e188c1dc1218066af
vincentcarbonaro <nd4spd356@gmail.com>
2015-04-08 17:30:09 -0400

    Up to finalize

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -1,22 +1,39 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
-# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
-# of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
@@ -32,7 +49,7 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    
   end
 
   def attributes
